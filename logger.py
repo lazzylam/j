7f733cd2baa2bb7d4368bb3b3d8c3f97003b705c
Lamora <itsmerick184@gmail.com>
2025-05-17 02:32:48 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,12 +1,7 @@
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - [%(levelname)s] - %(message)s",
-    handlers=[
-        logging.FileHandler("bot.log", encoding="utf-8"),
-        logging.StreamHandler()
-    ]
+    format='%(asctime)s - [%(levelname)s] - %(message)s',
+    level=logging.INFO
 )
-
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
