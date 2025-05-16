c82e212138a0b5c7831da7e65dc04f99252e2873
Lamora <itsmerick184@gmail.com>
2025-05-17 02:12:15 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+FORWARD_DELAY = int(os.getenv("FORWARD_DELAY", 30))
