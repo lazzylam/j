c48d17404e45fa4819522e95d83f085280011347
Lamora <itsmerick184@gmail.com>
2025-05-17 02:52:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 FORWARD_DELAY = int(os.getenv("FORWARD_DELAY", 30))
+
+print(f"[CONFIG] OWNER_ID = {OWNER_ID} (type: {type(OWNER_ID)})")
