437dcb18f6d91604667b66a3b3c25b81d7c9ad46
Lamora <itsmerick184@gmail.com>
2025-05-17 02:13:38 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
 from userbot.forwarder import trigger_forward
+from logger import logger
 
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -8,12 +9,18 @@ bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 async def handle_forwarded_msg(client, message):
     if not message.forward_from_chat:
         await message.reply("Gagal deteksi asal pesan.")
+        logger.warning("Forward gagal: Tidak ditemukan asal channel.")
         return
+
     await message.reply("Pesan diterima, mulai sebar ke grup...")
+    logger.info(f"Trigger forward: Pesan dari {message.forward_from_chat.title}")
+    
     await trigger_forward(message.forward_from_chat.id, message.forward_from_message_id)
+
     await message.reply("Forward selesai.")
+    logger.info("Forwarding selesai untuk satu pesan.")
 
 async def start_bot():
     await bot.start()
-    print("[BOT] Aktif")
+    logger.info("[BOT] Aktif")
     await bot.idle()
