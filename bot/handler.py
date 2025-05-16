55139cdba92b3aaa280fa50db0f77859e2716e4d
Lamora <itsmerick184@gmail.com>
2025-05-17 02:52:56 +0800

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -6,6 +6,11 @@ from logger import logger
 
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+@bot.on_message()
+async def debug_all(client, message):
+    from_user = message.from_user.id if message.from_user else "N/A"
+    logger.info(f"[DEBUG] Menerima pesan dari: {from_user}, forward: {bool(message.forward_from_chat)}")
+
 @bot.on_message(filters.private & filters.user(OWNER_ID))
 async def handle_any_msg(client, message):
     logger.info(f"[DEBUG] Menerima pesan dari owner: {message.text or 'non-text'}")
